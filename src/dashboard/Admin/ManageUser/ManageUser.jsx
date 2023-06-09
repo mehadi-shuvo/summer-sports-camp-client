7a6504fd0d19f64b3b6fac98b72f0b49ed114123
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-06-09 20:28:09 +0600

    added create admin handler

--- src/dashboard/Admin/ManageUser/ManageUser.jsx
+++ src/dashboard/Admin/ManageUser/ManageUser.jsx
@@ -1,9 +1,83 @@
+import { useQuery } from "react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaTrashAlt, FaUser, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const ManageUser = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
+        const res = await axiosSecure.get('/users/');
+        return res.data;
+    })
+
+
+    const handleCreateAdmin = user => {
+        fetch(`http://localhost:3000/users/admin/${user._id}`,{
+            method:'PATCH'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.modifiedCount>0){
+                refetch();
+                Swal.fire({
+                    position: 'top',
+                    icon: 'success',
+                    title: `Congratulations!! Now, ${user.name} is an admin.`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+
+    const handleRemoveCart = () => {
+
+    }
     return (
         <div className="my-20">
             <h4 className="text-4xl text-center font-bold text-orange-500">Manage User</h4>
-            
+            <h3>total {users.length}</h3>
+
+            <div>
+                <table className="table w-full">
+                    {/* head */}
+                    <thead className="">
+                        <tr className=" uppercase ">
+                            <th>#</th>
+                            <th>user name</th>
+                            <th>email</th>
+                            <th>role</th>
+                            <th>instructor</th>
+                            <th>admin</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    {user.name}
+                                </td>
+                                <td className="">{user.email}</td>
+                                <td className="">{user?.role ? user?.role : 'Student'}</td>
+                                <td >
+                                    <button className="btn myBtn bg-orange-500 hover:text-orange-500 ">Instructor</button>
+                                </td>
+                                <th>
+                                    <button 
+                                    disabled={user?.role === 'admin'}
+                                    onClick={()=>handleCreateAdmin(user)}
+                                    className="btn myBtn bg-orange-500 hover:text-orange-500 disabled:border-none">Admin</button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Route.jsx'
 import AuthProvider from './providers/AuthProvider/AuthProvider'
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
